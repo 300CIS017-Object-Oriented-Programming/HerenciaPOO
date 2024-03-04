2358f5e533b3b559f0d9b220e32583f2407be525
zalonore <zalonore@gmail.com>
2024-03-03 22:19:42 -0500

    cambio en comentarios del view 2 para que vuelva a correr

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,7 +5,7 @@ using std::cout;
 int main()
 {
     // Se instancia un Objeto de tipo View para enlazar con la vista
-    View vistaPPAL
+    View vistaPPAL;
     vistaPPAL.verPrincipal();
     return 0;
 }
