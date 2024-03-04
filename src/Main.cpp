02bbc7b1a24027ab34a978b0675136ee1d937be2
zalonore <zalonore@gmail.com>
2024-03-03 22:17:11 -0500

    cambio en comentarios del view

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,7 @@ using std::cout;
 
 int main()
 {
-    // Objeto de tipo View para enlazar con la vista
+    // Se instancia un Objeto de tipo View para enlazar con la vista
     View vistaPPAL;
     vistaPPAL.verPrincipal();
     return 0;
